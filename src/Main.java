db140754ef1306a700c7d08c3f0f7215b428c292
PaolaFerraro <91568657+PaolaFerraro@users.noreply.github.com>
2021-11-24 21:25:52 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
  * @authors 
  * Johan Sebastian Fontecha Soler   20191578026
  * Diana Paola Feraro Cardenas      20182020048
- * Andres Felipe Quintero Velazquez 20191020023  
+ * Andres Felipe Quintero Velasquez 20191020023  
  */
 import Control.*;
 
